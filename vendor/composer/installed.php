82444cafd08d98307526d651903198f118b1465e
mattsoh <mattsoh100@gmail.com>
2024-12-03 08:57:19 +0000

    add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mattsoh/advent-of-code',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e289aba487067ec07ad48909de1e5872d6fc9ffe',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mattsoh/advent-of-code' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e289aba487067ec07ad48909de1e5872d6fc9ffe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
