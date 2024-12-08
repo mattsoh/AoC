c0cd54d31b41e19cde973c61ffe568cef3f9e03b
mattsoh <mattsoh100@gmail.com>
2024-12-08 06:12:50 +0000

    change

--- src/database.php
+++ src/database.php
@@ -1,6 +1,12 @@
 <?php
 // src/database.php
 
+// Force HTTPS if not already using it
+if ($_SERVER['HTTPS'] != "on") {
+    header("Location: https://aoc.mattsoh.dev");
+    exit();
+}
+
 // Retrieve environment variables
 $dbHost = getenv('DB_HOST');
 $dbPort = getenv('DB_PORT');
@@ -8,16 +14,25 @@ $dbUser = getenv('DB_USER');
 $dbPass = getenv('DB_PASS');
 $dbName = getenv('DB_NAME');
 
+$ssl_ca = getenv('MYSQL_SSL_CA');
+$ssl_cert = getenv('MYSQL_SSL_CERT');
+$ssl_key = getenv('MYSQL_SSL_KEY');
+
 try {
-    // Data Source Name
+    // Data Source Name (DSN) for PDO with MySQL
     $dsn = "mysql:host=$dbHost;port=$dbPort;dbname=$dbName;charset=utf8mb4";
+    
+    // SSL Options for PDO
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_SSL_CA       => $ssl_ca,
+        PDO::MYSQL_ATTR_SSL_CERT     => $ssl_cert,
+        PDO::MYSQL_ATTR_SSL_KEY      => $ssl_key,
     ];
 
-    // Create a new PDO instance
+    // Create a new PDO instance with SSL connection options
     $db = new PDO($dsn, $dbUser, $dbPass, $options);
     // echo "Connected to the database successfully!<br>";
 
@@ -27,6 +42,7 @@ try {
         $db->exec("CREATE INDEX idx_leaderboard_total_score ON leaderboard (total_score DESC)");
     }
 } catch (PDOException $e) {
+    // Handle connection errors
     throw new PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
