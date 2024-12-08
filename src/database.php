f17fa5ed80693c10787cf1c040c7db35a572b48b
mattsoh <mattsoh100@gmail.com>
2024-12-08 06:27:33 +0000

    change

--- src/database.php
+++ src/database.php
@@ -23,7 +23,9 @@ try {
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
-        // Remove SSL options for testing
+        PDO::MYSQL_ATTR_SSL_CA       => $ssl_ca,
+        PDO::MYSQL_ATTR_SSL_CERT     => $ssl_cert,
+        PDO::MYSQL_ATTR_SSL_KEY      => $ssl_key,
     ];
 
     // Create a new PDO instance with SSL connection options
